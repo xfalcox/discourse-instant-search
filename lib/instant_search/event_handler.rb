1deb793e4e8b68a5560e080c3927b50c9f2065d2
Rafael Silva <xfalcox@gmail.com>
2024-05-06 15:06:25 -0300

    Fix event handling

--- lib/instant_search/event_handler.rb
+++ lib/instant_search/event_handler.rb
@@ -3,18 +3,122 @@
 module ::InstantSearch
   class EventHandler
     def self.setup(plugin)
-      ::InstantSearch::Collections::Base.subclasses.each do |collection|
-        symbol_prefix = collection.name.demodulize.underscore
+      # TODO use meta programming to DRY this mess up
+      plugin.on(:post_created) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::Post.name,
+          action: "upsert",
+        )
+      end
 
-        %w[created edited].each do |action|
-          plugin.on("#{symbol_prefix}_#{action}".to_sym) do |target|
-            Jobs.enqueue(:index, id: target.id, type: collection.name, action: "upsert")
-          end
-        end
+      plugin.on(:post_edited) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::Post.name,
+          action: "upsert",
+        )
+      end
 
-        plugin.on("#{symbol_prefix}_destroyed".to_sym) do |target|
-          Jobs.enqueue(:index, id: target.id, type: collection.name, action: "delete")
-        end
+      plugin.on(:post_destroyed) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::Post.name,
+          action: "delete",
+        )
+      end
+
+      plugin.on(:user_created) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::User.name,
+          action: "upsert",
+        )
+      end
+
+      plugin.on(:user_updated) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::User.name,
+          action: "upsert",
+        )
+      end
+
+      plugin.on(:user_destroyed) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::User.name,
+          action: "delete",
+        )
+      end
+
+      plugin.on(:topic_created) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::Topic.name,
+          action: "upsert",
+        )
+      end
+
+      plugin.on(:topic_updated) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::Topic.name,
+          action: "upsert",
+        )
+      end
+
+      plugin.on(:topic_destroyed) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::Topic.name,
+          action: "delete",
+        )
+      end
+
+      plugin.on(:chat_message_created) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::ChatMessage.name,
+          action: "upsert",
+        )
+      end
+
+      plugin.on(:chat_message_trashed) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::ChatMessage.name,
+          action: "delete",
+        )
+      end
+
+      plugin.on(:chat_message_edited) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::ChatMessage.name,
+          action: "upsert",
+        )
+      end
+
+      plugin.on(:chat_message_restored) do |target|
+        Jobs.enqueue(
+          :index,
+          id: target.id,
+          type: ::InstantSearch::Collections::ChatMessage.name,
+          action: "upsert",
+        )
       end
     end
   end
